815e5c3702ccdd0faa59e598c6c89eb3fbd8d22b
Joun <urbanscratcher@gmail.com>
2023-07-11 12:40:18 +0900

    added pdf conversion

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 # LaTex 관련 쉘 스크립트, 사용법 등을 기록
 * 사용 전 권한 주기
-    - chmod +x 스크립트명.sh
+  - chmod +x 스크립트명.sh
 
 ## tex2png.sh
 * tex 파일을 png로 변환
 * 사용법
-  - ./tex2png.sh 입력파일명.tex (출력파일명.png)
-  - 출력파일명을 미기재시 입력파일명.png로 출력
+  - ./tex2png.sh 입력파일명.tex (dvi)
+  - 옵션 인자로 dvi 기입시 dvi -> png 변환
+  - default는 pdf -> png
--- tex2png.sh
+++ tex2png.sh
@@ -1,22 +1,24 @@
 # Settings
 TEX_FILE="$1"
-OUTPUT_FILE="$2"
+OUTPUT_FILE="${TEX_FILE%.*}.png"
 RESOLUTION=600
+DVI_OR_PDF="$2"
 
 if [ -z "$TEX_FILE" ]; then
   echo "입력 파일명이 필요합니다."
   exit 1
 fi
 
-if [ -z "$OUTPUT_FILE" ]; then
-  OUTPUT_FILE="${TEX_FILE%.*}.png"
+if [ "$DVI_OR_PDF" == "dvi" ]; then
+  # Tex 2 DVI
+  latex "$TEX_FILE"
+
+  # DVI 2 PNG
+  dvipng -T tight -bg Transparent -D "$RESOLUTION" -o "$OUTPUT_FILE" "${TEX_FILE%.*}.dvi"
+else
+  pdflatex "$TEX_FILE"
+  convert -density 600 "${TEX_FILE%.*}.pdf" "$OUTPUT_FILE"
 fi
 
-# Tex 2 DVI
-latex "$TEX_FILE"
-
-# DVI 2 PNG
-dvipng -T tight -bg Transparent -D "$RESOLUTION" -o "$OUTPUT_FILE" "${TEX_FILE%.*}.dvi"
-
-# 중간 파일 정리 (선택사항)
-rm "${TEX_FILE%.*}.aux" "${TEX_FILE%.*}.dvi" "${TEX_FILE%.*}.log"
+# 중간 파일 삭제
+rm "${TEX_FILE%.*}.pdf" "${TEX_FILE%.*}.aux" "${TEX_FILE%.*}.dvi" "${TEX_FILE%.*}.log"
